da9e3380c7f3a693988af6514668e56948d89af9
Antonio Dias <accdias@gmail.com>
2026-01-07 12:21:12 -0300

    Improve readability

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,17 +5,20 @@
 
 -- Plugins {{{
 -- mini.vim {{{
-local path_package = vim.fn.stdpath('data') .. '/site'
-local mini_path = path_package .. '/pack/deps/start/mini.nvim'
+local pack_path = vim.fn.stdpath('data') .. '/site/pack'
+local mini_path = pack_path .. '/deps/start/mini.nvim'
+local mini_repo = 'https://github.com/nvim-mini/mini.vim'
 if not vim.loop.fs_stat(mini_path) then
   vim.cmd('echo "Installing `mini.nvim`" | redraw')
-  local clone_cmd = {
-    'git', 'clone', '--filter=blob:none',
-    -- Uncomment next line to use 'stable' branch
-    -- '--branch', 'stable',
-    'https://github.com/nvim-mini/mini.nvim', mini_path
+  local clone_repo = {
+      'git',
+      'clone',
+      '--filter=blob:none',
+      -- '--branch=stable',
+      mini_repo,
+      mini_path
   }
-  vim.fn.system(clone_cmd)
+  vim.fn.system(clone_repo)
   vim.cmd('packadd mini.nvim | helptags ALL')
   vim.cmd('echo "Installed `mini.nvim`" | redraw')
 end
